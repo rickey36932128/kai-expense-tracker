0d0ed0102c78027b86fa2d29d9927fe8fb3c6f38
rickey36932128 <rickey36932128@gmail.com>
2026-07-01 17:53:06 +0800

    Bump app version to v49

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const STORAGE_KEY = "kai-expense-tracker-v1";
-const APP_VERSION = "v48";
-const CACHE_REPAIR_KEY = "kai-cache-repair-v48";
+const APP_VERSION = "v49";
+const CACHE_REPAIR_KEY = "kai-cache-repair-v49";
 const UPDATE_CHECK_INTERVAL = 5 * 60 * 1000;
 const UPDATE_STATUS_HIDE_MS = 2200;
 const DEFAULT_CURRENCY = "TWD";
